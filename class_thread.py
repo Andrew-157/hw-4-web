f5185fe128fc3761048f55d90fe8e8adf8ec9686
Andrew Subotin <subotinandrey5@gmail.com>
2022-11-05 11:01:17 +0200

    re

--- class_thread.py
+++ class_thread.py
@@ -17,8 +17,9 @@ class File:
         shutil.move(original, move_to)
         splitted_name = self.name.split(".")
         new_name = normalize(self.name.replace(f'.{splitted_name[-1]}', ""))
-        os.rename(os.path.join(f'{self.dir_address}\\{self.sub_folder}', self.name), os.path.join(
-            f'{self.dir_address}\\{self.sub_folder}', f'{new_name}.{splitted_name[-1]}'))
+        sub_folder_path = os.path.join(self.dir_address, self.sub_folder)
+        os.rename(os.path.join(sub_folder_path, self.name), os.path.join(
+            sub_folder_path, f'{new_name}.{splitted_name[-1]}'))
 
 
 class Archive:
@@ -30,11 +31,12 @@ class Archive:
         splitted_name = self.name.split(".")
         new_name = normalize(
             self.name.replace(f'.{splitted_name[-1]}', ""))
+        to_unpack = os.path.join(self.dir_address, "archive")
         shutil.unpack_archive(os.path.join(self.dir_address, self.name),
-                              os.path.join(f'{self.dir_address}\\archive', new_name))
+                              os.path.join(to_unpack, new_name))
         os.remove(os.path.join(self.dir_address, self.name))
-        client_code(os.path.join(
-            f'{self.dir_address}\\archive', f'{new_name}\\archive'))
+        to_func = os.path.join(new_name, "archive")
+        client_code(os.path.join(to_unpack, to_func))
 
 
 def create_sub_folders(name):
@@ -52,34 +54,34 @@ def client_code(name):
     for file in dir_address.iterdir():
         if file.name in directories:
             continue
-        else:
-            if file.is_dir():
 
-                dir = os.listdir(file)
-                if len(dir) == 0:
-                    os.rmdir(os.path.join(dir_address, file.name))
-                else:
-                    client_code(os.path.join(dir_address, file.name))
+        if file.is_dir():
 
-            elif file.name.endswith(('.zip', 'gz', '.tar')):
-                arch = Archive(file.name, dir_address)
-                files.append(arch)
+            dir = os.listdir(file)
+            if not dir:
+                os.rmdir(os.path.join(dir_address, file.name))
+            else:
+                client_code(os.path.join(dir_address, file.name))
 
-            elif file.name.endswith(('.jpeg', '.png', '.jpg', '.svg')):
-                file_1 = File(file.name, dir_address, directories[0])
-                files.append(file_1)
+        elif file.name.endswith(('.zip', 'gz', '.tar')):
+            arch = Archive(file.name, dir_address)
+            files.append(arch)
 
-            elif file.name.endswith(('.avi', '.mp4', '.mov', '.mkv')):
-                file_2 = File(file.name, dir_address, directories[1])
-                files.append(file_2)
+        elif file.name.endswith(('.jpeg', '.png', '.jpg', '.svg')):
+            file_1 = File(file.name, dir_address, directories[0])
+            files.append(file_1)
 
-            elif file.name.endswith(('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx')):
-                file_3 = File(file.name, dir_address, directories[2])
-                files.append(file_3)
+        elif file.name.endswith(('.avi', '.mp4', '.mov', '.mkv')):
+            file_2 = File(file.name, dir_address, directories[1])
+            files.append(file_2)
 
-            elif file.name.endswith(('.mp3', '.ogg', '.wav', '.amr')):
-                file_4 = File(file.name, dir_address, directories[4])
-                files.append(file_4)
+        elif file.name.endswith(('.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx')):
+            file_3 = File(file.name, dir_address, directories[2])
+            files.append(file_3)
+
+        elif file.name.endswith(('.mp3', '.ogg', '.wav', '.amr')):
+            file_4 = File(file.name, dir_address, directories[4])
+            files.append(file_4)
 
     for file in files:
         Thread(target=file).start()
